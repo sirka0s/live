78d8e0098b91c93db83f37c07335c4da91de8659
sirka0s <73496111+sirka0s@users.noreply.github.com>
2021-01-05 15:31:23 +0100

    Added backend

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -2,6 +2,18 @@ provider "aws" {
     region = "us-east-1"
 }
 
+#Configuring backend for tfstate file
+terraform {
+  backend "s3" {
+      bucket = "kaos-terraform-state"
+      key = "global/iam/terraform.tfstate"
+      region = "us-east-1"
+
+      dynamodb_table = "kaos-terraform-state-lock"
+      encrypt = true
+  }
+}
+
 resource "aws_iam_user" "example" {
   for_each = toset(var.user_name)
   name = each.value
