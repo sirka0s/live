365d59555c35c2d3c21a6a726cdc04bcf54844d6
sirka0s <73496111+sirka0s@users.noreply.github.com>
2021-01-09 15:55:25 +0100

    Autoscaling as bool

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -15,7 +15,7 @@ terraform {
 }
 
 module "webserver_cluster" {
-    source = "github.com/sirka0s/modules//services/webserver-cluster?ref=v0.0.2"
+    source = "github.com/sirka0s/modules//services/webserver-cluster?ref=v0.0.6"
 
     cluster_name  = "webservers-stage"
     db_remote_state_bucket = "kaos-terraform-state"
@@ -24,5 +24,6 @@ module "webserver_cluster" {
     instance_type = "t2.micro" #Don't you fucking touch it
     min_size = 2
     max_size = 3
+    enable_autoscaling = false
 }
 
