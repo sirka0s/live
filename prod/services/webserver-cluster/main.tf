4c0f4d993a433934e204acb8f885be6a2b20831a
sirka0s <73496111+sirka0s@users.noreply.github.com>
2021-01-09 15:56:11 +0100

    Using local module file

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -15,7 +15,8 @@ terraform {
 }
 
 module "webserver_cluster" {
-    source = "github.com/sirka0s/modules//services/webserver-cluster?ref=v0.0.3"
+    source = "../../../../modules/services/webserver-cluster"
+    #source = "github.com/sirka0s/modules//services/webserver-cluster"
 
     cluster_name  = "webservers-prod"
     db_remote_state_bucket = "kaos-terraform-state"
@@ -25,28 +26,10 @@ module "webserver_cluster" {
     min_size = 2
     max_size = 10
 
+    enable_autoscaling = true
+
     custom_tagsy = {
       Owner = "kaos"
       DeployedBy = "terraform"
     }
-}
-
-resource "aws_autoscaling_schedule" "scale_out_during_bh" {
-  scheduled_action_name = "scale-out-during-business-hours"
-  min_size = 2
-  max_size = 10
-  desired_capacity = 10
-  recurrence = "0 9 * * * " # Every day at 9:00 AM
-
-  autoscaling_group_name = module.webserver_cluster.asg_name
-}
-
-resource "aws_autoscaling_schedule" "scale_in_at_night" {
-  scheduled_action_name = "scale-in-at-night"
-  min_size = 2
-  max_size = 10
-  desired_capacity = 2
-  recurrence = "0 17 * * * " # Every day at 5:00 PM
-
-  autoscaling_group_name = module.webserver_cluster.asg_name
 }
