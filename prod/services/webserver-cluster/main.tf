f510ed3052f2e7a854cdf866a1a6a50d852acf29
sirka0s <73496111+sirka0s@users.noreply.github.com>
2021-01-13 19:06:24 +0100

    Minor stuff

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -15,8 +15,8 @@ terraform {
 }
 
 module "webserver_cluster" {
-    source = "../../../../modules/services/webserver-cluster"
-    #source = "github.com/sirka0s/modules//services/webserver-cluster"
+    #source = "../../../../modules/services/webserver-cluster"
+    source = "github.com/sirka0s/modules//services/webserver-cluster"
 
     cluster_name  = "webservers-prod"
     db_remote_state_bucket = "kaos-terraform-state"
